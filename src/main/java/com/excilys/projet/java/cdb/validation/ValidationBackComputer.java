e6d9ebfc038d046db34b67901a254c1501e2e796
Varlet <cedric.varlet@efrei.net>
2020-07-01 17:53:08 +0200

    Ajout validator

--- src/main/java/com/excilys/projet/java/cdb/validation/ValidationBackComputer.java
+++ src/main/java/com/excilys/projet/java/cdb/validation/ValidationBackComputer.java
@@ -1,5 +1,24 @@
 package com.excilys.projet.java.cdb.validation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ValidationBackComputer 
 {
+	private static Logger logger = LoggerFactory.getLogger(ValidationBackComputer.class);
+
+    public static boolean dateFormatValidator(String date) {
+        boolean dateIsValid = true;
+        try {
+            LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            dateIsValid = false;
+            logger.error("invalid date format: " + date);
+        } catch (NullPointerException e) {
+            logger.info("the date is null");
+        }
+        return dateIsValid;
+    }
 }
